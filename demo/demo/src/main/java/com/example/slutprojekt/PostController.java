4dc0611155c6255f696471735e99574e982bc79a
RR <rebecca.rivera@awacademy.training>
2022-10-20 14:54:20 +0200

    funkar ej

--- demo/demo/src/main/java/com/example/slutprojekt/PostController.java
+++ demo/demo/src/main/java/com/example/slutprojekt/PostController.java
@@ -276,25 +276,27 @@ public class PostController {
 
     @GetMapping("/filesUpload")
     public String filesUpload(HttpSession session,Principal principal, Model model,HttpServletRequest request, @RequestParam(value = "page", required = false, defaultValue = "1") int page) {
-
         Document document = new Document();
-        List<AllFiles> allFiles = new ArrayList<AllFiles>();
+
+        List<AllFiles> allFiles = new ArrayList<>();
 
         for (Document document1 : documentRepo.findAll()) {
             allFiles.add(new AllFiles(document1.getFileName(), document1.getDate()));
 
+          //  allFilesRepo.save(new AllFiles(document1.getFileName(), document1.getDate()));
         }
         for (Assignment document1 : assignmentRepo.findAll()) {
             allFiles.add(new AllFiles(document1.getFileName(), document1.getCreatedDate()));
-
+           // allFilesRepo.save(new AllFiles(document1.getFileName(), document1.getCreatedDate()));
         }
         for (TeacherAnnouncement document1 : teacherAnnouncementRepo.findAll()) {
             allFiles.add(new AllFiles(document1.getImg(), document1.getDate()));
-
+           // allFilesRepo.save(new AllFiles(document1.getImg(), document1.getDate()));
         }
 
+
         model.addAttribute("document", document);
-        model.addAttribute("fil", allFiles);
+             model.addAttribute("fil", allFiles);
 
         return "filesUpload";
     }
